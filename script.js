bf2027e777fda033dbbc88b3b7510ce64ebf018a
Evelyn Gomez <evelyn.gomez15@gmail.com>
2022-05-09 11:14:50 -0700

    edit svg file, added css pointer-0 & X, single square eventListener'

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let gameboard = document.querySelector('.gameboard'); 
 let all_single_squares = document.querySelectorAll('.gameboard div'); 
 
+
+function checkIfAssignedAMarker(){
+
+}
 for(let singleSquare of all_single_squares){
   console.log(singleSquare);
   singleSquare.addEventListener('pointerdown', (event)=> {
-    event.target.classList.add('pointer-O'); 
+    let isMarkerAssigned = checkIfAssignedAMarker(event.target);
+    if (isMarkerAssigned){
+      //return error -- has already a marker
+    }else{
+      //assign marker
+      event.target.classList.add('pointer-X'); 
+    }
     // singleSquare.innerHtml = 'X';  
   });
 };
